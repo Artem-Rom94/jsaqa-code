ad17e3356780eb5b778c2f5bc451a919cd43a801
Artem test <artem-klasssss@mail.ru>
2026-09-07 21:23:01 +0300

    Добавлены тесты

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -7,11 +7,30 @@ describe("Books names test suit", () => {
         "Гарри Поттер",
         "Властелин Колец",
         "Волшебник изумрудного города",
-      ])
+      ]),
     ).toEqual([
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
     ]);
+  });
+
+  it("Should return same array if already sorted", () => {
+    const input = ["Азбука", "Букварь", "Грамматика"];
+    expect(sorting.sortByName(input)).toEqual([
+      "Азбука",
+      "Букварь",
+      "Грамматика",
+    ]);
+  });
+
+  it("Should be case-insensitive", () => {
+    const input = ["b", "A", "c"];
+    expect(sorting.sortByName(input)).toEqual(["A", "b", "c"]);
+  });
+
+  it("Should handle equal names", () => {
+    const input = ["a", "a"];
+    expect(sorting.sortByName(input)).toEqual(["a", "a"]);
   });
 });
