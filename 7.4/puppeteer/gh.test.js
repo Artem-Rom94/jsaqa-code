c4805c9618fc1233fcf34cbbd75164f298be29da
Artem test <artem-klasssss@mail.ru>
2026-09-10 12:05:44 +0300

    убрано дублирование

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,15 +1,20 @@
 let page;
 
+
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
   page.close();
 });
 
+
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -34,16 +39,8 @@ describe("Github page tests", () => {
   }, 30000);
 });
 
-// ===== НОВЫЙ =====
+//  НОВЫЙ БЛОК
 describe("Other GitHub pages title tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-  });
-
-  afterEach(() => {
-    page.close();
-  });
-
   test("GitHub main page title should contain 'GitHub'", async () => {
     await page.goto("https://github.com");
     const title = await page.title();
